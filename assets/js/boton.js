9746eeaa5c620abeb592be0df7ffea7fcc098621
danielchocho97 <60704244+danielchocho97@users.noreply.github.com>
2022-06-28 11:03:06 -0500

    initial commit

---
+++ assets/js/boton.js
@@ -0,0 +1,92 @@
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+////
+
+jQuery(window).scroll(function(){
+    var $sections = $('section');
+    $sections.each(function(i,el){
+        var top  = $(el).offset().top-100;
+        var bottom = top +$(el).height();
+        var scroll = $(window).scrollTop();
+        var id = $(el).attr('id');
+        if( scroll > top && scroll < bottom){
+            $('a.active-nav-link').removeClass('active-nav-link');
+            $('a[href="#'+id+'"]').addClass('active-nav-link');
+
+        }
+    })
+});
+
+$("nav").on("click","a", function (event) {
+
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 800);
+});
+
+/*crorrusel */
+if(document.querySelector('#container-slider')){
+  setInterval('fntExecuteSlide("next")',5000);
+}
+//------------------------------ LIST SLIDER -------------------------
+if(document.querySelector('.listslider')){
+  let link = document.querySelectorAll(".listslider li a");
+  link.forEach(function(link) {
+     link.addEventListener('click', function(e){
+        e.preventDefault();
+        let item = this.getAttribute('itlist');
+        let arrItem = item.split("_");
+        fntExecuteSlide(arrItem[1]);
+        return false;
+     });
+   });
+}
+
+function fntExecuteSlide(side){
+   let parentTarget = document.getElementById('slider');
+   let elements = parentTarget.getElementsByTagName('li');
+   let curElement, nextElement;
+
+   for(var i=0; i<elements.length;i++){
+
+       if(elements[i].style.opacity==1){
+           curElement = i;
+           break;
+       }
+   }
+   if(side == 'prev' || side == 'next'){
+
+       if(side=="prev"){
+           nextElement = (curElement == 0)?elements.length -1:curElement -1;
+       }else{
+           nextElement = (curElement == elements.length -1)?0:curElement +1;
+       }
+   }else{
+       nextElement = side;
+       side = (curElement > nextElement)?'prev':'next';
+
+   }
+   //RESALTA LOS PUNTOS
+   let elementSel = document.getElementsByClassName("listslider")[0].getElementsByTagName("a");
+   elementSel[curElement].classList.remove("item-select-slid");
+   elementSel[nextElement].classList.add("item-select-slid");
+   elements[curElement].style.opacity=0;
+   elements[curElement].style.zIndex =0;
+   elements[nextElement].style.opacity=1;
+   elements[nextElement].style.zIndex =1;
+}
